a8db784a5571f3c2dfb44f4e132bd8c7e616401c
sangeer-lama <sangeer.negi@gmail.com>
2020-10-12 12:44:51 +0545

    Solved Build issue regarding directory missing

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,27 +1,19 @@
-using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        List<Employee> employee =new List<Employee>();
-        var employee1= new Employee
-        {
-            FirstName="Bishnu",
-            SurName="Rawal",
-            Address="Ktm",
-            Salary=100.0;
-        }
-        return View();
-
+        var employees = Employee.GetEmployees();        
+        return View(employees);
     }
-}
-public class Employee 
-{
-    public string FirstName{get;set;}
-    public string SurName{get;set;}
-    public string Address{get;set;}
-    public char Gender{get;set;}
-    public doubles Salary{get;set;}
+
+    public ActionResult Detail(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
+        
+        return View(employee);
+    }
 }
---
+++ EmployeeManagement/Models/employee.cs
@@ -0,0 +1,25 @@
+
+using System.Collections.Generic;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public string Address { get; set; }
+    public char Gender { get; set; } = 'M';
+    public double Salary { get; set; }
+
+    public static List<Employee> GetEmployees()
+    {
+        // Object initializer
+        var employee1 = new Employee { FirstName = "Top", Surname = "Shrestha", Address = "Ktm", Salary = 10500.0 };
+        var employee2 = new Employee { FirstName = "Shyam", Surname = "Basnet", Address = "Bhaktapur", Salary = 105000.0 };
+        var employee3 = new Employee { FirstName = "Bijay", Surname = "Nepal", Address = "Dang", Salary = 9000.0 };
+        var employee4 = new Employee { FirstName = "Pritam", Surname = "Khatri", Address = "Chitwan", Salary = 11100.0 };
+        var employee5 = new Employee { FirstName = "NiKhil", Surname = "Uppreti", Address = "Lalitpur", Salary = 8500.0 };
+        var employee6 = new Employee { FirstName = "Binay", Surname = "Bajracharya", Address = "Ktm", Salary = 26000.0 };
+
+        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
+        return employees;     
+    }
+}
